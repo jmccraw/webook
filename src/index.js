f341028f51bd0234e7887032a17a1e91c409daef
Justin McCraw <jmccraw1@gmail.com>
2023-07-27 14:32:54 -0400

    feat: update the App as more a homepage we redirect from, and update index for routing

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  redirect,
+  RouterProvider
+} from 'react-router-dom';
+
+import Rooms from './features/rooms';
+
+import './styles/reset.css';
+import './styles/vars.css';
+import './styles/defaults.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    loader: () => redirect('/rooms') // we only have one route, so just redirect to it
+  },
+  {
+    path: '/rooms',
+    element: (
+      <Rooms />
+    )
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
